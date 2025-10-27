0289244d2bbb38a5c81eb788793a1b0d8c1d582a
SoSaddy <86183342+SoSaddy@users.noreply.github.com>
2025-10-27 23:05:39 +0500

    Add files via upload

--- Timer.cpp
+++ Timer.cpp
@@ -1,74 +1,47 @@
-#include <Timer.h>
+#include "Timer.h"
 
 Timer::Timer(uint32_t cooldown, uint8_t mode) {
-
 	this->_time = millis();
 	this->_cooldown = cooldown;
 	this->_mode = mode;
-	this->_switched = false;
-
-};
+}
 
 bool Timer::get() {
+	return _getState(this->_cooldown);
+}
 
-	bool state = (millis() - this->_time >= this->_cooldown);
+bool Timer::get(uint32_t &cooldown) {
+	return _getState(cooldown);
+}
 
-	if (this->_mode == AS_TRIGGER) {
-
-		if (state) {
-			this->_time = millis();
-		}
-		return state;
-
-	} else if (this->_mode == AS_SWITCH) {
-
-		if (state) {
-			this->_switched = !this->_switched;
-			this->_time = millis();
-		}
-		return this->_switched;
-
-	}
-
-	return false;
-	
-};
-
-bool Timer::get(uint32_t cooldown) {
-
+bool Timer::_getState(const uint32_t &cooldown) {
 	bool state = (millis() - this->_time >= cooldown);
 
 	if (this->_mode == AS_TRIGGER) {
-
 		if (state) {
 			this->_time = millis();
 		}
 		return state;
 
 	} else if (this->_mode == AS_SWITCH) {
-
 		if (state) {
 			this->_switched = !this->_switched;
 			this->_time = millis();
 		}
 		return this->_switched;
-
 	}
 
 	return false;
-	
-};
-
+}
 
 void Timer::setCooldown(uint32_t cooldown) {
 	this->_cooldown = cooldown;
-
-};
+}
 
 void Timer::setMode(uint8_t mode) {
 	this->_mode = mode;
-};
+}
 
 void Timer::clear() {
 	this->_time = millis();
-};
+}
--- Timer.h
+++ Timer.h
@@ -8,19 +8,21 @@ const uint8_t AS_TRIGGER = 1;
 
 class Timer {
 
-	public:
-		Timer(uint32_t cooldown = 1000, uint8_t mode = AS_TRIGGER);
-		bool get();
-		bool get(uint32_t cooldown);
-		void setCooldown(uint32_t cooldown);
-		void setMode(uint8_t mode);
-		void clear();
-
 	private:
 		uint32_t _time;
 		uint32_t _cooldown;
 		uint8_t _mode;
-		bool _switched;
+		bool _switched = false;
+		bool _getState(const uint32_t &cooldown);
+
+	public:
+		Timer(uint32_t cooldown = 1000, uint8_t mode = AS_TRIGGER);
+		bool get();
+		bool get(uint32_t &cooldown);
+		void setCooldown(uint32_t cooldown);
+		void setMode(uint8_t mode);
+		void clear();
+
 };
 
 #endif
